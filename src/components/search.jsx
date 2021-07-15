bced43394eae8a5703e9fd0b10254348b52067b3
SevagAvedissian <sevag.avedissian@gmail.com>
2021-07-15 16:15:52 -0700

    fixed search

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -4,45 +4,55 @@ import JSONDATA from "../data.json";
 import { Icon, IconSize } from "@blueprintjs/core";
 
 const Search = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  return (
-    <div className="container3">
-      <span>
-        <Icon className="m-1" icon="globe" iconSize={20} />
-        <input
-          className="search"
-          type="text"
-          placeholder="Search..."
-          onChange={(event) => {
-            setSearchTerm(event.target.value);
-          }}
-        />{" "}
-      </span>
+    const [searchTerm, setSearchTerm] = useState("");
+    return (
+        <div className="container3">
+            <span>
+                <Icon className="m-1" icon="globe" iconSize={20} />
+                <input
+                    className="search"
+                    type="text"
+                    placeholder="Search..."
+                    onChange={(event) => {
+                        setSearchTerm(event.target.value);
+                    }}
+                />{" "}
+            </span>
 
-      {JSONDATA.filter((val) => {
-        const nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-        if (searchTerm === "") return val;
-        //else if (val.ITEM.toLowerCase().includes(searchTerm.toLowerCase()))
-        //return val;
-        if (val.foodItems <= searchTerm) return val;
-        else if (
-          val.ITEM.toLowerCase().includes(searchTerm.toLowerCase()) &&
-          !/\d/.test(searchTerm) //val.ITEM.some(nums) //includes("1") //fix this
-        )
-          return val;
-      }).map((val, key) => {
-        return (
-          <div className="user " key={key}>
-            {" "}
-            <p className=" container-fluid">
-              {val.ITEM} <span className="badge bg-dark">{val.CAL} </span>
-            </p>
-          </div>
-        );
-      })}
-    </div>
-  );
+            {
+            JSONDATA.map((value, key) => {
+              //console.log(value);
+              //for(let i = 0; i < 5; i++){}
+                value.foodItems.filter((val) => {
+                    //console.log(val.foodItems.lenght);
+                    if (searchTerm === "") return val;
+                    //else if (val.ITEM.toLowerCase().includes(searchTerm.toLowerCase()))
+                    //return val;
+                    if (val.calories <= searchTerm)
+                        return val;
+                    else if (
+                        val.foodName.toLowerCase().includes(
+                            searchTerm.toLowerCase()
+                        ) &&
+                        !/\d/.test(searchTerm) //val.ITEM.some(nums) //includes("1") //fix this
+                    )
+                        return val;
+                }).map((val, key) => {
+                    return (
+                        <div className="user " key={key}>
+                            {" "}
+                            <p className=" container-fluid">
+                                {val.foodName}{" "}
+                                <span className="badge bg-dark">
+                                    {val.calories}{" "}
+                                </span>
+                            </p>
+                        </div>
+                    );
+                });
+            })}
+        </div>
+    );
 };
 
 export default Search;
