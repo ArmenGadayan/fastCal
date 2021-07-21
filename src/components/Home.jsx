f7d4a59c847baaa857372049e6f5f8263556b1dd
SevagAvedissian <sevag.avedissian@gmail.com>
2021-07-20 17:04:51 -0700

    row and column

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,20 +3,40 @@ import React, { Component } from "react";
 import Restaurant from "./restaurant";
 
 class Home extends Component {
-  render() {
-    const { restaurants, onPress } = this.props;
-    return (
-      <div>
-        {restaurants.map((res) => (
-          <Restaurant
-            name={res.id}
-            pic={res.link}
-            onPress={onPress}
-          ></Restaurant>
-        ))}
-      </div>
-    );
-  }
+    render() {
+        const { restaurants, onPress } = this.props;
+        // return (
+        //   <div>
+        //     {restaurants.map((res) => (
+        //       <Restaurant
+        //         name={res.id}
+        //         pic={res.link}
+        //         onPress={onPress}
+        //       ></Restaurant>
+        //     ))}
+        //   </div>
+        // );
+        const rows = [...Array(Math.ceil(restaurants.length / 3))];
+        // chunk the products into the array of rows
+        const productRows = rows.map((row, idx) =>
+            restaurants.slice(idx * 3, idx * 3 + 3)
+        );
+        // map the rows as div.row
+        const content = productRows.map((row, idx) => (
+            <div className="row" key={idx}>
+                {row.map((res) => (
+                    <div className="col-md-3">
+                        <Restaurant
+                            name={res.id}
+                            pic={res.link}
+                            onPress={onPress}
+                        ></Restaurant>
+                    </div>
+                ))}
+            </div>
+        ));
+        return <div classname="table">{content}</div>;
+    }
 }
 
 export default Home;
--- src/components/restaurant.jsx
+++ src/components/restaurant.jsx
@@ -2,17 +2,20 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class Restaurant extends Component {
-  render() {
-    return (
-      <div className="resLogo">
-        <Link to="/search" onClick={() => this.props.onPress(this.props.name)}>
-          {/* <button className="btn btn-info btn-sm m-2">search</button> */}
-          <img src={this.props.pic} alt="Logo" width="15%" height="50%" />
-          <div>{this.props.name}</div>
-        </Link>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="resLogo">
+                <Link
+                    to="/search"
+                    onClick={() => this.props.onPress(this.props.name)}
+                >
+                    {/* <button className="btn btn-info btn-sm m-2">search</button> */}
+                    <img src={this.props.pic} alt="Logo" />
+                    <div>{this.props.name}</div>
+                </Link>
+            </div>
+        );
+    }
 }
 
 export default Restaurant;
