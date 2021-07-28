3df27ead0ed32231afda0aa5880833436b2a32ac
SevagAvedissian <sevag.avedissian@gmail.com>
2021-07-27 17:57:45 -0700

    a

--- src/components/sidebarContent.jsx
+++ src/components/sidebarContent.jsx
@@ -4,33 +4,35 @@ import deleteIcon from "../icons/delete.svg";
 import addIcon from "../icons/add.svg";
 
 const SidebarContent = ({ foodList, onDeleteItem, onAddItem }) => {
-  return (
-    <div>
-      {foodList.map((item) => {
-        return (
-          <div className="sideBarLine">
-            <span className="badge rounded-pill bg-dark m-2">
-              {item.quantity}{" "}
-            </span>
-            {item.name}{" "}
-            <span className="sideBarCal">| {item.calories} cals</span>
-            <input
-              className="quantityBox1"
-              type="image"
-              src={deleteIcon}
-              onClick={() => onDeleteItem(item)}
-            />
-            <input
-              className="quantityBox2"
-              type="image"
-              src={addIcon}
-              onClick={() => onAddItem(item.name, item.calories)}
-            />
-          </div>
-        );
-      })}
-    </div>
-  );
+    return (
+        <div>
+            {foodList.map((item) => {
+                return (
+                    <div className="sideBarLine qty mt-5">
+                        <span
+                            className="minus bg-dark"
+                            onClick={() => onAddItem(item.name, item.calories)}
+                        >
+                            +
+                        </span>
+                        <span className="badge rounded-pill bg-secondary m-2">
+                            {item.quantity}{" "}
+                        </span>
+                        <span
+                            className="minus bg-dark"
+                            onClick={() => onDeleteItem(item)}
+                        >
+                            -
+                        </span>
+
+                        <span className="sideBarCal m-2">
+                            {item.name} | {item.calories} cals
+                        </span>
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default SidebarContent;
