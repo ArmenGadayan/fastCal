3f9eef0c45e7d0b9be2bd42ca5509c3a47906ff5
ArmenGadayan <armengadayan@gmail.com>
2021-07-30 17:08:25 -0700

    j

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -6,43 +6,41 @@ import SidebarContent from "./sidebarContent";
 import { Icon, IconSize } from "@blueprintjs/core";
 
 const Sidebar = ({ foodList, onDeleteItem, onAddItem }) => {
-    const [show, setShow] = useState("");
-    const handleClose = () => setShow(false);
-    const toggleShow = () => setShow((s) => !s);
+  const [show, setShow] = useState("");
+  const handleClose = () => setShow(false);
+  const toggleShow = () => setShow((s) => !s);
 
-    const options = {
-        scroll: false,
-        backdrop: true,
-    };
+  const options = {
+    scroll: false,
+    backdrop: true,
+  };
 
-    return (
-        <React.Fragment>
-            <button
-                variant="primary"
-                onClick={toggleShow}
-                className="menuButton"
-            >
-                <Icon icon="menu" />
-            </button>{" "}
-            <Offcanvas
-                show={show}
-                onHide={handleClose}
-                scroll={true}
-                backdrop={false}
-            >
-                <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>Food List</Offcanvas.Title>
-                </Offcanvas.Header>
-                <Offcanvas.Body>
-                    <SidebarContent
-                        foodList={foodList}
-                        onDeleteItem={onDeleteItem}
-                        onAddItem={onAddItem}
-                    />
-                </Offcanvas.Body>
-            </Offcanvas>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <button variant="primary" onClick={toggleShow} className="menuButton">
+        <Icon icon="menu" />
+      </button>{" "}
+      <Offcanvas
+        show={show}
+        onHide={handleClose}
+        scroll={true}
+        backdrop={false}
+      >
+        <span className="listNav">
+          <Offcanvas.Header closeButton closeVariant="white">
+            <Offcanvas.Title>Food List</Offcanvas.Title>
+          </Offcanvas.Header>{" "}
+        </span>
+        <Offcanvas.Body>
+          <SidebarContent
+            foodList={foodList}
+            onDeleteItem={onDeleteItem}
+            onAddItem={onAddItem}
+          />
+        </Offcanvas.Body>
+      </Offcanvas>
+    </React.Fragment>
+  );
 };
 
 export default Sidebar;
